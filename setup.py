74073394bb7364f76055179762e72a1c1f8c7c60
nikanick11 <nnadiradze@chegg.com>
2024-08-02 15:18:33 -0400

    Change graphql-core requirement to <3.4

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     keywords="api graphql protocol rest relay graphene",
     packages=find_packages(exclude=["examples*"]),
     install_requires=[
-        "graphql-core>=3.1,<3.3",
+        "graphql-core>=3.1,<3.4",
         "graphql-relay>=3.1,<3.3",
     ],
     tests_require=tests_require,
